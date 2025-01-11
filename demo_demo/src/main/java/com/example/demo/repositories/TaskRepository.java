75734dfaf13c5c61863f4e12cf9f2a2b65c30c37
ala-kad <kaddechiala@gmail.com>
2025-01-11 22:17:05 +0100

    Task Repository Interface

---
+++ demo_demo/src/main/java/com/example/demo/repositories/TaskRepository.java
@@ -0,0 +1,13 @@
+package com.example.demo.repositories;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+
+import com.example.demo.entities.Task;
+
+public interface TaskRepository extends CrudRepository<Task, Long> {
+	List<Task> findAll(); 
+	List<Task> findByStatus(boolean status);
+}
