b77b659be51148dbadb439b954b7c0d08b1e606a
ala-kad <kaddechiala@gmail.com>
2025-01-15 18:08:15 +0100

    Added missing repository methods(filtering using status, sorting by creation-date)

--- demo_demo/src/main/java/com/example/demo/repositories/TaskRepository.java
+++ demo_demo/src/main/java/com/example/demo/repositories/TaskRepository.java
@@ -8,5 +8,10 @@ import org.springframework.data.repository.CrudRepository;
 import com.example.demo.entities.Task;
 
 public interface TaskRepository extends CrudRepository<Task, Long> {
-	List<Task> findAll(); 
+	List<Task> findAll();
+	List<Task> findByCompleted(Boolean completed);
+	List<Task> findByCompletedOrderByCreationDateAsc(Boolean completed);
+    List<Task> findByCompletedOrderByCreationDateDesc(Boolean completed);
+    List<Task> findAllByOrderByCreationDateAsc();
+    List<Task> findAllByOrderByCreationDateDesc(); 
 }
